7cfa177d1f5fce797bef1dd60d8ea4d6a0ec1760
Carter Supple <Cartersupple123@gmail.com>
2022-02-27 11:21:35 -0800

    fixing bugs

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/budgettracker";
 
 const app = express();
 
@@ -16,14 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+app.use(require("./routes/api.js"));
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/budgettracker",
+{
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true
 });
-
-// routes
-app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
